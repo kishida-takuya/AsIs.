6da541110eaae84a61a95545da9b8a945182b278
takuya.kishida <kishida-takuya@agex.co.jp>
2022-08-01 01:11:31 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-             <h1>Keep your Diary with "AsIs."</h1>
-            {{-- ユーザ登録ページへのリンク --}}
-            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
         </div>
-        <div class="text-center">
-            <h2>Not "To be", But "As is".<br>It's your real diary, "AsIs."></h2>
-        </div>
-    </div>
+    @endif
 @endsection
